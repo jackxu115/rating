ae54548e41da4d406e792bb8ccfeecd1c8ae10df
pengxu <pengxu19890115@gmail.com>
2019-11-21 19:33:38 -0800

    start function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Star = ({ selected = false, onClick = f => f }) => (
+  <div className={selected ? 'star selected' : 'star'} onClick={onClick}></div>
+);
+
+Star.propTypes = {
+  selected: PropTypes.bool,
+  onClick: PropTypes.func
+};
 
 function App() {
-  return (
-    <div className="App">
-      <p>Edit and save to reload.</p>
-    </div>
-  );
+  return <Star selected={true} onClick={() => console.log('Star Click')} />;
 }
 
 export default App;
