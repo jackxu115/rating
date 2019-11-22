455a49e152ba522d57acadbcf7b31fc8ee6f6ec0
pengxu <pengxu19890115@gmail.com>
2019-11-21 19:53:31 -0800

    star rating func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Star.css';
 
@@ -11,8 +11,48 @@ Star.propTypes = {
   onClick: PropTypes.func
 };
 
-function App() {
-  return <Star selected={true} onClick={() => console.log('Star Click')} />;
+class StarRating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      starsSelected: 0
+    };
+    this.change = this.change.bind(this);
+  }
+
+  change(starsSelected) {
+    this.setState({ starsSelected });
+  }
+
+  render() {
+    const { totalStars } = this.props;
+    const { starsSelected } = this.state;
+    return (
+      <div className="star-rating">
+        {[...Array(totalStars)].map((n, i) => (
+          <Star
+            key={i}
+            selected={i < starsSelected}
+            onClick={() => this.change(i + 1)}
+          />
+        ))}
+        <p>
+          {starsSelected} of {totalStars} stars
+        </p>
+      </div>
+    );
+  }
 }
+
+function App() {
+  return <StarRating />;
+}
+
+StarRating.propTypes = {
+  totalStars: PropTypes.number
+};
+StarRating.defaultProps = {
+  totalStars: 5
+};
 
 export default App;
